13f276ba990048a63a5d0f05ac8bc12510b8696d
jordan-435 <52468983+jordan-435@users.noreply.github.com>
2021-06-05 16:10:50 -0500

    MainCharacter

---
+++ ZOVID-21/Assets/playerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Animator anim;
+    private Rigidbody rb;
+    public LayerMask layerMsk;
+    public bool grounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Grounded();
+        Jump();
+        Move();
+    }
+
+    private void Grounded()
+    {
+        if(Physics.CheckSphere(this.transform.position + Vector3.down, 0.2f, layerMsk))
+        {
+            this.grounded = true;
+        }
+        else
+        {
+            this.grounded = false;
+        }
+
+        anim.SetBool("jump", this.grounded);
+    }
+
+
+
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && this.grounded)
+        {
+            this.rb.AddForce(Vector3.up * 4, ForceMode.Impulse);
+        }
+
+    }
+
+    private void Move()
+    {
+        float verticalAxis = Input.GetAxis("Vertical");
+        float horizontalAxis = Input.GetAxis("Horizontal");
+
+        Vector3 movement = this.transform.forward * verticalAxis + this.transform.right * horizontalAxis;
+        movement.Normalize();
+
+        this.transform.position += movement * 0.04f;
+
+        anim.SetFloat("vertical", verticalAxis);
+        anim.SetFloat("horizontal", horizontalAxis);
+    }
+}
---
+++ ZOVID-21/Assets/playerMovemont.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovemont : MonoBehaviour
+{
+
+    public float WalkSpeed;
+    public float RunSpeed;
+    public float CurrentSpeed;
+
+    CharacterController controller;
+    private Vector3 offSet;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        RunSpeed = WalkSpeed + 7;
+        CurrentSpeed = WalkSpeed;
+    }
+    void Update()
+    {
+
+        PlayerMovement();
+
+    }
+
+    public bool PlayerMovement()
+    {
+
+        float hor = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        if (Input.GetMouseButton(1) || Input.GetMouseButton(0))
+        {
+            CurrentSpeed = WalkSpeed;
+        }
+        else if (((hor != 0) || (vert != 0)) && Input.GetKey("left shift"))
+        {
+            CurrentSpeed = RunSpeed;
+        }
+        else if (Input.GetKeyUp("left shift"))
+        {
+            CurrentSpeed = WalkSpeed;
+        }
+
+
+        Vector3 playerMovement = new Vector3(hor, 0f, vert) * CurrentSpeed * Time.deltaTime;
+
+        transform.Translate(playerMovement, Space.Self);
+
+        return true;
+
+    }
+}
+
