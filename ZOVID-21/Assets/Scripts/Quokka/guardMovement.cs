fcf48a3996e72b9f250526128399b991c4ee901d
jordan-435 <52468983+jordan-435@users.noreply.github.com>
2021-06-08 13:20:19 -0500

    Latest Update
    Got pretty far with this one

---
+++ ZOVID-21/Assets/Scripts/Quokka/guardMovement.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guardMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public float waitTime = .3f;
+    public float turnspeed = 90;
+
+    public Transform pathHolder;
+    public GameObject player;
+
+    public Light spotlight;
+    public float viewDistance;
+    public LayerMask viewMask;
+    float viewAngle;
+    Color originalSpotlightColor;
+
+    public Animator anim;
+
+    private void Start()
+    {
+        originalSpotlightColor = spotlight.color;
+        player = GameObject.FindGameObjectWithTag("Player");
+        viewAngle = spotlight.spotAngle;
+
+        Vector3[] waypoints = new Vector3[pathHolder.childCount];
+        for(int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = pathHolder.GetChild(i).position;
+            waypoints[i] = new Vector3(waypoints[i].x, transform.position.y, waypoints[i].z);
+        }
+        StartCoroutine(FollowPath(waypoints));
+
+    }
+    void Update()
+    {
+        if(player != null)
+        {
+            if (CanSeePlayer())
+            {
+                spotlight.color = Color.red;
+                BlastThatMf();
+            }
+        }
+        else
+        {
+            spotlight.color = originalSpotlightColor;
+        }
+    }
+
+    void BlastThatMf()
+    {
+        Destroy(player,5);
+    }
+
+
+    bool CanSeePlayer()
+    {
+
+        if (Vector3.Distance(transform.position, player.transform.position) < viewDistance)
+        {
+            Vector3 dirToPlayer = (player.transform.position - transform.position).normalized;
+            float angleBetweenGuardAndPlayer = Vector3.Angle(transform.forward, dirToPlayer);
+            if (angleBetweenGuardAndPlayer < viewAngle / 2f)
+            {
+                if (!Physics.Linecast(transform.position, player.transform.position, viewMask))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    IEnumerator FollowPath(Vector3[] waypoints)
+    {
+        transform.position = waypoints[0];
+        int targetWaypointIndex = 1;
+        Vector3 targetWaypoint = waypoints[targetWaypointIndex];
+        transform.LookAt(targetWaypoint);
+
+        while (true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
+            anim.SetFloat("vertical", 1);
+            if (transform.position == targetWaypoint)
+            {
+                Debug.Log("Here!!!!!");
+                anim.SetFloat("vertical", 0);
+                targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
+                targetWaypoint = waypoints[targetWaypointIndex];
+                yield return new WaitForSeconds(waitTime);
+                yield return StartCoroutine(TurnToFace(targetWaypoint));
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator TurnToFace(Vector3 lookTarget)
+    {
+        Vector3 dirToLookTarget = (lookTarget - transform.position).normalized;
+        float targetAngle = 90 - Mathf.Atan2(dirToLookTarget.z, dirToLookTarget.x) * Mathf.Rad2Deg;
+
+        while (Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle)) > 0.05f)
+        {
+            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnspeed * Time.deltaTime);
+            transform.eulerAngles = Vector3.up * angle;
+            yield return null;
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Vector3 startPosition = pathHolder.GetChild(0).position;
+        Vector3 previousPosition = startPosition;
+        foreach (Transform waypoint in pathHolder) 
+        {
+            Gizmos.DrawSphere(waypoint.position, .3f);
+            Gizmos.DrawLine(previousPosition, waypoint.position);
+            previousPosition = waypoint.position;
+        }
+        Gizmos.DrawLine(previousPosition, startPosition);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward * viewDistance);
+    }
+}
---
+++ ZOVID-21/Assets/Scripts/Zombie/playerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 7;
+    public float smoothMoveTime = .1f;
+    public float turnSpeed = 8;
+
+    float angle;
+
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
+    Vector3 velocity;
+
+    new Rigidbody rigidbody;
+    private GameObject soonToBeSeptic;
+    public float strikingDistance = 2f;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        soonToBeSeptic = GameObject.FindGameObjectWithTag("Antiseptic");
+
+
+    }
+
+    void Update()
+    {
+        Vector3 inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        float inputMagnitude = inputDirection.magnitude;
+        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+
+        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
+
+        velocity = transform.forward * moveSpeed * smoothInputMagnitude;
+        CheckStrikingRange();
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.deltaTime);
+    }
+
+    void CheckStrikingRange()
+    {
+        if(soonToBeSeptic != null)
+        {
+            Vector3 positionDied = soonToBeSeptic.transform.position;
+            if (Vector3.Distance(soonToBeSeptic.transform.position, transform.position) < strikingDistance)
+            {
+                Debug.Log("Here!!!");
+                Destroy(soonToBeSeptic);
+                Instantiate(this.gameObject, positionDied, Quaternion.identity);
+            }
+        }
+
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, strikingDistance);
+    }
+}
